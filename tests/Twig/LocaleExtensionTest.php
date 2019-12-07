65dc7a594ddad782802cdfdc83285cfd54c923a6
Artem Rodygin <artem.rodygin@gmail.com>
2019-12-07 14:04:05 +1300

    Translations

---
+++ tests/Twig/LocaleExtensionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\Twig;
+
+use PHPUnit\Framework\TestCase;
+use Twig\TwigFilter;
+
+/**
+ * @coversDefaultClass \eTraxis\Twig\LocaleExtension
+ */
+class LocaleExtensionTest extends TestCase
+{
+    /**
+     * @covers ::getFilters
+     */
+    public function testFilters()
+    {
+        $expected = [
+            'direction',
+        ];
+
+        $extension = new LocaleExtension();
+
+        $filters = array_map(function (TwigFilter $filter) {
+            return $filter->getName();
+        }, $extension->getFilters());
+
+        self::assertSame($expected, $filters);
+    }
+
+    /**
+     * @covers ::filterDirection
+     */
+    public function testFilterDirection()
+    {
+        $extension = new LocaleExtension();
+
+        self::assertSame(LocaleExtension::LEFT_TO_RIGHT, $extension->filterDirection('en'));
+        self::assertSame(LocaleExtension::RIGHT_TO_LEFT, $extension->filterDirection('ar'));
+        self::assertSame(LocaleExtension::RIGHT_TO_LEFT, $extension->filterDirection('fa'));
+        self::assertSame(LocaleExtension::RIGHT_TO_LEFT, $extension->filterDirection('he'));
+    }
+}
