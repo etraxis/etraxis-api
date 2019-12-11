5e4ba5278b9c01f474c88a0d0efafbd7adb5a904
Artem Rodygin <artem.rodygin@gmail.com>
2019-12-11 21:31:44 +1300

    Mailer

---
+++ src/Subscriber/MessageSender.php
@@ -0,0 +1,74 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\Event\MessageEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Message;
+
+/**
+ * Sets sender info for each outgoing email.
+ */
+class MessageSender implements EventSubscriberInterface
+{
+    private $sender;
+
+    /**
+     * @codeCoverageIgnore Dependency Injection constructor.
+     *
+     * @param string $sender
+     */
+    public function __construct(string $sender)
+    {
+        $this->sender = $sender;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            MessageEvent::class => 'onMessage',
+        ];
+    }
+
+    /**
+     * Sets sender info for each outgoing email.
+     *
+     * @param MessageEvent $event
+     */
+    public function onMessage(MessageEvent $event)
+    {
+        $message = $event->getMessage();
+
+        if (!$message instanceof Message) {
+            return;
+        }
+
+        $headers = $message->getHeaders();
+
+        if (!$headers->has('from')) {
+            $headers->addMailboxListHeader('from', [new Address($this->sender, 'eTraxis')]);
+        }
+
+        if (!$headers->has('reply-to')) {
+            $from = $headers->get('from')->getBody();
+            $headers->addMailboxListHeader('reply-to', $from);
+        }
+
+        $message->setHeaders($headers);
+    }
+}
---
+++ tests/Subscriber/MessageSenderTest.php
@@ -0,0 +1,171 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+/** @noinspection PhpInternalEntityUsedInspection */
+
+namespace eTraxis\Subscriber;
+
+use eTraxis\WebTestCase;
+use Symfony\Component\Mailer\DelayedEnvelope;
+use Symfony\Component\Mailer\Event\MessageEvent;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport\NullTransport;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\RawMessage;
+
+/**
+ * @coversDefaultClass \eTraxis\Subscriber\MessageSender
+ */
+class MessageSenderTest extends WebTestCase
+{
+    /**
+     * @var \Symfony\Component\Mailer\MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * @var MessageSender
+     */
+    private $subscriber;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mailer     = new Mailer(new NullTransport());
+        $this->subscriber = new MessageSender('noreply@example.com');
+    }
+
+    /**
+     * @covers ::getSubscribedEvents
+     */
+    public function testGetSubscribedEvents()
+    {
+        $expected = [
+            MessageEvent::class,
+        ];
+
+        self::assertSame($expected, array_keys(MessageSender::getSubscribedEvents()));
+    }
+
+    /**
+     * @covers ::onMessage
+     */
+    public function testHasFrom()
+    {
+        $email = new Email();
+
+        $email
+            ->from('sender@example.com')
+            ->to('recipient@example.com')
+            ->subject('Test message')
+            ->text('Message body');
+
+        $event = new MessageEvent($email, new DelayedEnvelope($email), 'smtp://null');
+
+        $this->subscriber->onMessage($event);
+
+        /** @var Email $message */
+        $message = $event->getMessage();
+
+        /** @var \Symfony\Component\Mime\Address[] $from */
+        $from = $message->getFrom();
+        self::assertSame('sender@example.com', $from[0]->getAddress());
+
+        /** @var \Symfony\Component\Mime\Address[] $replyTo */
+        $replyTo = $message->getReplyTo();
+        self::assertSame('sender@example.com', $replyTo[0]->getAddress());
+    }
+
+    /**
+     * @covers ::onMessage
+     */
+    public function testHasFromAndReplyTo()
+    {
+        $email = new Email();
+
+        $email
+            ->from('sender@example.com')
+            ->replyTo('reply@example.com')
+            ->to('recipient@example.com')
+            ->subject('Test message')
+            ->text('Message body');
+
+        $event = new MessageEvent($email, new DelayedEnvelope($email), 'smtp://null');
+
+        $this->subscriber->onMessage($event);
+
+        /** @var Email $message */
+        $message = $event->getMessage();
+
+        /** @var \Symfony\Component\Mime\Address[] $from */
+        $from = $message->getFrom();
+        self::assertSame('sender@example.com', $from[0]->getAddress());
+
+        /** @var \Symfony\Component\Mime\Address[] $replyTo */
+        $replyTo = $message->getReplyTo();
+        self::assertSame('reply@example.com', $replyTo[0]->getAddress());
+    }
+
+    /**
+     * @covers ::onMessage
+     */
+    public function testNoFrom()
+    {
+        $email = new Email();
+
+        $email
+            ->to('recipient@example.com')
+            ->subject('Test message')
+            ->text('Message body');
+
+        $event = new MessageEvent($email, new DelayedEnvelope($email), 'smtp://null');
+
+        $this->subscriber->onMessage($event);
+
+        /** @var Email $message */
+        $message = $event->getMessage();
+
+        /** @var \Symfony\Component\Mime\Address[] $from */
+        $from = $message->getFrom();
+        self::assertSame('noreply@example.com', $from[0]->getAddress());
+
+        /** @var \Symfony\Component\Mime\Address[] $replyTo */
+        $replyTo = $message->getReplyTo();
+        self::assertSame('noreply@example.com', $replyTo[0]->getAddress());
+    }
+
+    /**
+     * @covers ::onMessage
+     */
+    public function testNotMessage()
+    {
+        $email = new Email();
+
+        $email
+            ->from('sender@example.com')
+            ->to('recipient@example.com')
+            ->subject('Test message')
+            ->text('Message body');
+
+        $event = new MessageEvent(new RawMessage($email->toIterable()), new DelayedEnvelope($email), 'smtp://null');
+
+        $this->subscriber->onMessage($event);
+
+        $message = $event->getMessage();
+
+        foreach ($message->toIterable() as $entry) {
+            self::assertNotRegExp('/Reply\-To\: /', $entry);
+        }
+    }
+}
