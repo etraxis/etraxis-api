629b0a3f0ced4e40a3b4ab852b411085f821dd9a
Artem Rodygin <artem.rodygin@gmail.com>
2019-12-19 22:08:21 +1300

    Abstract voter + Validator translations

---
+++ src/Voter/AbstractVoter.php
@@ -0,0 +1,66 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\Voter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+/**
+ * Abstract voter that helps to check for supported attributes.
+ */
+abstract class AbstractVoter extends Voter
+{
+    /**
+     * @var array List of supported attributes. Each key of the array is an attribute name,
+     *            value - class of the subject (use 'null' if subject is not required).
+     *
+     * Example:
+     *
+     * protected $attributes = [
+     *     'create' => null,
+     *     'update' => MyEntity::class,
+     *     'delete' => AnotherEntity::class,
+     * ];
+     */
+    protected $attributes = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports(string $attribute, $subject)
+    {
+        // Whether the attribute is supported.
+        if (!array_key_exists($attribute, $this->attributes)) {
+            return false;
+        }
+
+        $expectedClass = $this->attributes[$attribute];
+
+        // Whether the subject is not required.
+        if ($subject === null && $expectedClass === null) {
+            return true;
+        }
+
+        // The subject must be an object.
+        if (!is_object($subject)) {
+            return false;
+        }
+
+        // Subject may be a Doctrine Proxy class,
+        // e.g. 'Proxies\__CG__\App\Entity\MyEntity' instead of 'App\Entity\MyEntity'.
+        $class = mb_substr(get_class($subject), -mb_strlen($expectedClass));
+
+        // The subject must be of expected class.
+        return $class === $expectedClass;
+    }
+}
---
+++ tests/Voter/AbstractVoterTest.php
@@ -0,0 +1,100 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2018 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+namespace eTraxis\Voter;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\User\User;
+
+/**
+ * @coversDefaultClass \eTraxis\Voter\AbstractVoter
+ */
+class AbstractVoterTest extends TestCase
+{
+    /**
+     * @var Voter
+     */
+    private $voter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->voter = new class() extends AbstractVoter {
+            // Supported attributes.
+            protected $attributes = [
+                'create' => null,
+                'update' => User::class,
+            ];
+
+            protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
+            {
+                return true;
+            }
+        };
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testSupportedAttribute()
+    {
+        $object = new User('artem', 'secret');
+
+        /** @var TokenInterface $token */
+        $token = self::createMock(TokenInterface::class);
+
+        self::assertSame(VoterInterface::ACCESS_GRANTED, $this->voter->vote($token, null, ['create']));
+        self::assertSame(VoterInterface::ACCESS_GRANTED, $this->voter->vote($token, $object, ['update']));
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testUnsupportedAttribute()
+    {
+        $object = new User('artem', 'secret');
+
+        /** @var TokenInterface $token */
+        $token = self::createMock(TokenInterface::class);
+
+        self::assertSame(VoterInterface::ACCESS_ABSTAIN, $this->voter->vote($token, null, ['unknown']));
+        self::assertSame(VoterInterface::ACCESS_ABSTAIN, $this->voter->vote($token, $object, ['unknown']));
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testMissingClass()
+    {
+        /** @var TokenInterface $token */
+        $token = self::createMock(TokenInterface::class);
+
+        self::assertSame(VoterInterface::ACCESS_GRANTED, $this->voter->vote($token, null, ['create']));
+        self::assertSame(VoterInterface::ACCESS_ABSTAIN, $this->voter->vote($token, null, ['update']));
+    }
+
+    /**
+     * @covers ::supports
+     */
+    public function testWrongClass()
+    {
+        /** @var TokenInterface $token */
+        $token = self::createMock(TokenInterface::class);
+
+        self::assertSame(VoterInterface::ACCESS_ABSTAIN, $this->voter->vote($token, new \stdClass(), ['update']));
+    }
+}
